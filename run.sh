9f34ee45c1d69b5148ada8b03176b71107791d48
JMDirksen <jeftadirksen@gmail.com>
2024-01-17 11:38:49 +0100

    show env vars on start

--- run.sh
+++ run.sh
@@ -13,4 +13,16 @@ echo $FORWARD8 >> /etc/postfix/virtual
 echo $FORWARD9 >> /etc/postfix/virtual
 echo $FORWARD10 >> /etc/postfix/virtual
 postmap /etc/postfix/virtual
+echo "Hostname:   $HOSTNAME"
+echo "Domains:    $DOMAINS"
+echo "Forward  1: $FORWARD1"
+echo "Forward  2: $FORWARD2"
+echo "Forward  3: $FORWARD3"
+echo "Forward  4: $FORWARD4"
+echo "Forward  5: $FORWARD5"
+echo "Forward  6: $FORWARD6"
+echo "Forward  7: $FORWARD7"
+echo "Forward  8: $FORWARD8"
+echo "Forward  9: $FORWARD9"
+echo "Forward 10: $FORWARD10"
 postfix -v start-fg
